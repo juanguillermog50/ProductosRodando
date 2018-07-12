55a6d6d9bcd3296130c1b426e231d238673f0521
juanguillermog50 <juanguillermog50@gmail.com>
2018-07-12 09:21:05 -0500

    Código faltante

---
+++ ProductosRodando/Service/WcfDataService1.svc.cs
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using ProductosRodando.Models;
+
+namespace ProductosRodando.Service {
+  public class WcfDataService1 : DataService< PedidosDBEntities1 > {
+    // This method is called only once to initialize service-wide policies.
+    public static void InitializeService(DataServiceConfiguration config) {
+      // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+      // Examples:
+      // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+      // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+      config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+      config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+      config.UseVerboseErrors = true;
+    }
+  }
+}
